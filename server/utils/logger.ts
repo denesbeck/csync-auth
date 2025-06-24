8e7b1bbc4ce6afc4e9eef460896a0888e1d5e9c9
denesbeck <denesbeck@denesbeck-MacBook-Pro.local>
2025-06-24 19:56:00 +0200

    fix: rm apos from logger msg

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -6,26 +6,38 @@ export default {
     console.error(
       chalk.dim(format(new Date(), "yyyy-MM-dd HH:mm:ss")) +
         chalk.red(" [ERROR] ") +
-        args.map((el) => JSON.stringify(el)).join(" "),
+        args
+          .map((el) => JSON.stringify(el))
+          .join(" ")
+          .slice(1, -1),
     );
   },
   warn: (...args: any[]) =>
     console.warn(
       chalk.dim(format(new Date(), "yyyy-MM-dd HH:mm:ss")) +
         chalk.yellow(" [WARN] ") +
-        args.map((el) => JSON.stringify(el)).join(" "),
+        args
+          .map((el) => JSON.stringify(el))
+          .join(" ")
+          .slice(1, -1),
     ),
   debug: (...args: any[]) =>
     Boolean(process.env.DEBUG) &&
     console.debug(
       chalk.dim(format(new Date(), "yyyy-MM-dd HH:mm:ss")) +
         chalk.blue(" [DEBUG] ") +
-        args.map((el) => JSON.stringify(el)).join(" "),
+        args
+          .map((el) => JSON.stringify(el))
+          .join(" ")
+          .slice(1, -1),
     ),
   info: (...args: any[]) =>
     console.info(
       chalk.dim(format(new Date(), "yyyy-MM-dd HH:mm:ss")) +
         chalk.cyan(" [INFO] ") +
-        args.map((el) => JSON.stringify(el)).join(" "),
+        args
+          .map((el) => JSON.stringify(el))
+          .join(" ")
+          .slice(1, -1),
     ),
 };
